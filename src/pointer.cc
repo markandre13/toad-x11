354ddcdddbc8d4e50b41d94f15640339400ad2a6
Mark-Andre Hopf <mhopf@mark13.org>
2003-06-27 17:35:32 +0000

    Discovered the stupid assumption that all objects on the heap can be deleted. Which is wrong for objects being member variables. Reverted to the smart pointer implementation used till 2002-11-10 and added a range check to handle cases the TSmartObject isn't the first one in the list of super classes and 'this' isn't equal to the memory allocated by the overloaded new operator.

--- src/pointer.cc
+++ src/pointer.cc
@@ -1,6 +1,6 @@
 /*
  * TOAD -- A Simple and Powerful C++ GUI Toolkit for the X Window System
- * Copyright (C) 1996-2003 by Mark-André Hopf <mhopf@mark13.de>
+ * Copyright (C) 1996-2002 by Mark-André Hopf <mhopf@mark13.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,83 +19,6 @@
  */
 
 #include <toad/pointer.hh>
-#include <iostream>
 
-using namespace toad;
-
-/**
- * \ingroup smartpointer
- * \class toad::TSmartObject
- *
- * Classes whose objects shall be referenced by smartpointers must
- * inherit from this class.
- *
- * \todo
- *   \li
- *     Does someone know of a better way to detect whether the
- *     object was allocated on the stack? the current method
- *     looks dirty and isn't even thread safe.
- */
-
-static char * stack_head;
-enum stacktype_t {
-  STACK_UNKNOWN,
-  STACK_DOWN,
-  STACK_UP
-};
-static enum stacktype_t stack_type = STACK_UNKNOWN;
-
-/**
- * Initialize the algorithm which determined whether an object is on
- * the stack or on the heap.
- *
- * All TSmartObject's created before invocation of this method aren't
- * destroyed when they aren't referenced anymore. This is a sane
- * behaviour for static objects but might miss some objects allocated
- * by constructors.
- *
- * \param head
- *   Pointer to an object on the stack. (toad::initialize uses the address
- *   of 'argv' from the main function.)
- */
-void
-TSmartObject::initialize(void *head)
-{
-  char b;
-  stack_head = (char*)head;
-  if (stack_head > &b) {
-    stack_type = STACK_DOWN;
-  } else {
-    stack_type = STACK_UP;
-  }
-}
-
-TSmartObject::TSmartObject()
-{
-  _toad_ref_cntr = 0;
-
-  char dummy;
-  char *p = (char*)this;
-  
-  switch(stack_type) {
-    case STACK_DOWN:
-      if (stack_head >= p && p>=&dummy)
-        _toad_ref_cntr = notheap;
-      break;
-    case STACK_UP:
-      if (stack_head <= p && p<=&dummy)
-        _toad_ref_cntr = notheap;
-      break;
-    case STACK_UNKNOWN:
-      _toad_ref_cntr = notheap;
-      break;
-  }
-}
-
-TSmartObject::~TSmartObject() {
-  if (_toad_ref_cntr!=0 && _toad_ref_cntr!=notheap) {
-    cerr << "warning: object with pending references destroyed" << endl;
-    // Another idea would be to find all associated smart pointers
-    // and set 'em to NULL or to throw an exception
-  }
-}
+void* toad::TSmartObject::heap_start = 0;
+void* toad::TSmartObject::heap_end = 0;
--- src/pointer.hh
+++ src/pointer.hh
@@ -1,6 +1,6 @@
 /*
  * TOAD -- A Simple and Powerful C++ GUI Toolkit for the X Window System
- * Copyright (C) 1996-2003 by Mark-André Hopf <mhopf@mark13.de>
+ * Copyright (C) 1996-2002 by Mark-André Hopf <mhopf@mark13.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -46,7 +46,7 @@
  * ptr2 = ptr1
  * ptr3 = ptr2;
  *
- * ptr1 = 0;
+ * ptr1 = NULL;
  * ptr2 = ptr3;
  * ptr3 = new MyClass();
  * ptr2 = ptr3;
@@ -96,18 +96,64 @@
  * loose their reference on the object.
  */
 
+#include <malloc.h>
+#include <cassert>
+#include <iostream>
+
 namespace toad {
 
 using namespace std;
 
+/**
+ * @ingroup smartpointer
+ *
+ * Classes whose objects shall be referenced by smartpointers must
+ * inherit from this class.
+ */
 class TSmartObject
 {
-    static const unsigned notheap = (unsigned)-1;
+    static const unsigned nodelete = (unsigned)-1;
   public:
-    TSmartObject();
-    static void initialize(void *head);
+    static void* heap_start;
+    static void* heap_end;
+  
+    TSmartObject() {
+      // we do a range check because this object might to have
+      // been this first one in the list of super classes
+      if (heap_start && heap_start<=this && this<=heap_end) {
+        heap_start = 0;
+        _toad_ref_cntr = 0;
+      } else {
+        _toad_ref_cntr = nodelete;
+      }
+    }
     
-    virtual ~TSmartObject();
+    /**
+     * This method is called, when a smart object is created
+     * with new, which means that a smart pointer is allowed to
+     * delete the object.
+     */
+    void * operator new(std::size_t size) {
+      assert(heap_start == NULL);
+      char * ptr = new char[size];
+      heap_start = ptr;
+      heap_end   = ptr+size;
+      return ptr;
+    }
+
+    void * operator new(std::size_t n, void* p) {
+      cout << "new2 TSmartObject" << endl;
+      return p;
+    }
+    
+    virtual ~TSmartObject() {
+      if (_toad_ref_cntr!=0 && _toad_ref_cntr!=nodelete) {
+        cerr << "warning: object with pending references destroyed" << endl;
+        // Another idea would be to find all associated smart pointers
+        // and set 'em to NULL or to throw an exception
+      }
+    }
+//  private:
     unsigned _toad_ref_cntr;
 };
 
@@ -120,33 +166,33 @@ template <class T>
 class GSmartPointer
 {
   private:
-    static const unsigned _onstack = (unsigned)-1;
+    static const unsigned nodelete = (unsigned)-1;
     
   public:
     /* A reference count template that is used to encapsulate an object */
   
     GSmartPointer() {
-      _ptr = 0;
+      _ptr = NULL;
     }
 
     GSmartPointer(T* p) {
-      _ptr = 0;
+      _ptr = NULL;
       _set(p);
     }
 
     GSmartPointer(const GSmartPointer<T>& p) {
-      _ptr = 0;
+      _ptr = NULL;
       _set(p._ptr);
     }
 
     template <class T2>
     GSmartPointer(const GSmartPointer<T2>& p) {
-      _ptr = 0;
+      _ptr = NULL;
       _set(p._ptr);
     }
 
     ~GSmartPointer() {
-      _set(0);
+      _set(NULL);
     }
     
     GSmartPointer<T>& operator =(T *p) {
@@ -182,13 +228,13 @@ class GSmartPointer
     void _set(T *p) {
       if (_ptr==p)
         return;
-      if (_ptr && _ptr->_toad_ref_cntr!=_onstack) {
+      if (_ptr && _ptr->_toad_ref_cntr!=nodelete) {
         _ptr->_toad_ref_cntr--;
         if (_ptr->_toad_ref_cntr==0)
           delete _ptr;
       }
       _ptr = p;
-      if (_ptr && _ptr->_toad_ref_cntr!=_onstack)
+      if (_ptr && _ptr->_toad_ref_cntr!=nodelete)
         _ptr->_toad_ref_cntr++;
     }
 };
