d7aea5559df6d426484a953e7d8b5a6abb46d43f
Mark-Andre Hopf <mhopf@mark13.org>
2003-07-10 05:31:31 +0000

    added the options '--function-name', '--prefix', '--help' and '--' to support multiple C resource files (ie. for inclusion for TFileDialog.atv or TFontDialog.atv into the library itself)

--- src/bin2c.c
+++ src/bin2c.c
@@ -1,6 +1,6 @@
 /*
  * TOAD -- A Simple and Powerful C++ GUI Toolkit for the X Window System
- * Copyright (C) 1996-2002 by Mark-André Hopf <mhopf@mark13.de>
+ * Copyright (C) 1996-2003 by Mark-André Hopf <mhopf@mark13.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -19,37 +19,87 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv)
+static const char * prgname = "TOAD C++ GUI Library: Binary to C converter 1.2\n";
+
+static void 
+usage()
 {
-	int i, x, byte;
+  printf(
+    "%s"
+    "Usage: toadbin2c [OPTION] FILE ...\n"
+    "\n"
+    "Valid command line options are:\n"
+    "--function-name <name>\n"
+    "    specifies the function name used to initialize the memory files\n"
+    "    default value is 'createMemoryFiles'\n"
+    "--prefix <prefix>\n"
+    "    an additional prefix for the memory filenames\n"
+    "--\n"
+    "    last option, all following arguments are treated as files\n"
+    "--help\n"
+    "    display this help and exit\n"
+    "\n"
+    "Please report bugs or feedback to <toad-bug@mark13.de>\n",
+    prgname
+  );
+  exit(0);
+}
+
+int
+main(int argc, char **argv)
+{
+	int i, j, x, byte;
 	unsigned size;
 	FILE *in;
+	
+	char * functionname = "createMemoryFiles";
+	char * prefix       = "";
 
-#if 0
-	if (argc==1) {
-		printf("TOAD binary to C converter 1.1\n");
-		return 0;
-	}
-#endif
+  for(i=1; i<argc; ++i) {
+    if (strcmp(argv[i], "--")==0) {
+      ++i;
+      break;
+    }
+    if (strcmp(argv[i], "--help")==0) {
+      usage();
+    } else
+    if (strcmp(argv[i], "--function-name")==0) {
+      if (argc-i<1) {
+        fprintf(stderr, "not enough arguments for %s\n", argv[i]);
+        return EXIT_FAILURE;
+      }
+      functionname = argv[++i];
+    } else
+    if (strcmp(argv[i], "--prefix")==0) {
+      if (argc-i<1) {
+        fprintf(stderr, "not enough arguments for %s\n", argv[i]);
+        return EXIT_FAILURE;
+      }
+      prefix = argv[++i];
+    } else
+      break;
+  }
 
-	printf("/* Generated automatically from various files by bin2c. */\n"
+	printf("/* Generated automatically from various files by toadbin2c. */\n"
 				 "\n"
 				 "#include <toad/io/urlstream.hh>\n"
 				 "using namespace toad;\n"
 				 "\n"
-				 "void createMemoryFiles()\n"
-				 "{\n");
-	
-	for(i=1; i<argc; i++) {
+				 "void %s()\n"
+				 "{\n", functionname);
+
+	for(j=0; i<argc; i++, j++) {
 		in = fopen(argv[i], "r");
 		if (!in) {
 			perror(argv[i]);
-			return 1;
+			return EXIT_FAILURE;
 		}
 		
-		printf("  /* file: %s */\n", argv[i]);
-		printf("  static const char data%04i[] = {\n", i);
+		printf("  /* file: %s%s */\n", prefix, argv[i]);
+		printf("  static const char data%04i[] = {\n", j);
 				
 		size = 0;
 		x = 0;
@@ -72,11 +122,11 @@ int main(int argc, char **argv)
 		fclose(in);
 		
 		printf("\n  };\n");
-		printf("  urlstreambase::saveMemoryFile(\"%s\", data%04i, %u);\n\n",
-			argv[i], i, size);
+		printf("  urlstreambase::saveMemoryFile(\"%s%s\", data%04i, %u);\n\n",
+			prefix, argv[i], j, size);
 	}
 	
 	printf("}\n");
 	
-	return 0;
+	return EXIT_SUCCESS;
 }
