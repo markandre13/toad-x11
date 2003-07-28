4aa274040ebef3a0439513f53797cb0af847b473
Mark-Andre Hopf <mhopf@mark13.org>
2003-07-28 19:00:47 +0000

    added TException class with stacktrace support

---
+++ src/exception.hh
@@ -0,0 +1,53 @@
+/*
+ * TOAD -- A Simple and Powerful C++ GUI Toolkit for the X Window System
+ * Copyright (C) 1996-2003 by Mark-André Hopf <mhopf@mark13.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,   
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ * MA  02111-1307,  USA
+ */
+
+#ifndef _TOAD_EXCEPTION_HH
+#define _TOAD_EXCEPTION_HH
+
+#include <toad/stacktrace.hh>
+
+#include <exception>
+#include <string>
+#include <iostream>
+
+namespace toad {
+
+class TException:
+  public exception
+{
+      std::string msg;
+      TStackTrace stack;
+    public:
+      explicit TException(const std::string &what):exception(), msg(what) {}
+      virtual TException::~TException() throw() { }
+
+      void printStackTrace() const { stack.print(); }
+      void print() const {
+        cerr << msg << endl;
+        stack.print();
+      }
+
+      const std::string& getMessage() const { return msg; }
+      const char * what() const throw() { return msg.c_str(); }
+};
+
+} // namespace toad
+
+#endif
