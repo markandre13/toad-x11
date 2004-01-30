141dc2f0fbad7fc093ebab425dc5d271de55e9d6
Mark-Andre Hopf <mhopf@mark13.org>
2004-01-30 13:21:26 +0000

    removed obsolete 'TResource' class declaration which broke usage of this name in applications

--- src/control.hh
+++ src/control.hh
@@ -1,6 +1,6 @@
 /*
  * TOAD -- A Simple and Powerful C++ GUI Toolkit for the X Window System
- * Copyright (C) 1996-2003 by Mark-André Hopf <mhopf@mark13.de>
+ * Copyright (C) 1996-2004 by Mark-André Hopf <mhopf@mark13.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -26,8 +26,6 @@
 #include <toad/window.hh>
 
 namespace toad {
-
-class TResource;
 
 class TControl: 
   public TWindow
