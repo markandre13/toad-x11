563be9ca6e920d72d22c39ea23764563a68c8d59
Mark-Andre Hopf <mhopf@mark13.org>
2004-07-25 03:13:00 +0000

    fix: bitmaps where invalid after the first usage of the color dialog

--- src/colordialog.cc
+++ src/colordialog.cc
@@ -1,6 +1,6 @@
 /*
  * TOAD -- A Simple and Powerful C++ GUI Toolkit for the X Window System
- * Copyright (C) 1996-2004 by Mark-André Hopf <mhopf@mark13.de>
+ * Copyright (C) 1996-2004 by Mark-André Hopf <mhopf@mark13.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -50,8 +50,10 @@ TColorDialog::TColorDialog(TWindow *parent, const string &title, TRGB *aColor):
 
 TColorDialog::~TColorDialog()
 {
+/*
   delete bmp1;
   delete bmp2;
+*/
 }
 
 static void
