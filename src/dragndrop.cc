cdccc395c181de33ce01809195c6e9fbc9a13276
Mark-Andre Hopf <mhopf@mark13.org>
2004-09-25 11:35:29 +0000

    added the contents of X11R6's 'xc/lib/Xmu/ClientWin.c' file to TOAD to avoid linking with libXmu, libSM, libICE and libXt

--- src/dragndrop.cc
+++ src/dragndrop.cc
@@ -18,6 +18,35 @@
  * MA  02111-1307,  USA
  */
 
+/* 
+The contents of the file 'xc/lib/Xmu/ClientWin.c' are within this source
+file to avoid inclusion of libXmu, libSM, libICE and libXt.
+
+Copyright 1989, 1998  The Open Group
+
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that
+the above copyright notice appear in all copies and that both that
+copyright notice and this permission notice appear in supporting
+documentation.
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+OPEN GROUP BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of The Open Group shall not be
+used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from The Open Group.
+
+*/
+
+
 /**
  * @defgroup dnd Drag'n Drop
  *
@@ -330,6 +359,96 @@ DBM(cout << __FILE__ << ":" << __LINE__ << endl;)
   // try a handle position right here!
 }
 
+/* 
+ 
+Copyright 1989, 1998  The Open Group
+
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that
+the above copyright notice appear in all copies and that both that
+copyright notice and this permission notice appear in supporting
+documentation.
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+OPEN GROUP BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of The Open Group shall not be
+used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from The Open Group.
+
+*/
+
+/*
+ * Prototypes
+ */
+static Window TryChildren(Display*, Window, Atom);
+
+/* Find a window with WM_STATE, else return win itself, as per ICCCM */
+
+static Window
+XmuClientWindow(Display *dpy, Window win)
+{
+  Atom WM_STATE;
+  Atom type = None;
+  int format;
+  unsigned long nitems, after;
+  unsigned char *data = NULL;
+  Window inf;
+
+  WM_STATE = XInternAtom(dpy, "WM_STATE", True);
+  if (!WM_STATE)
+    return win;
+  XGetWindowProperty(dpy, win, WM_STATE, 0, 0, False, AnyPropertyType,
+                     &type, &format, &nitems, &after, &data);
+  if (data)
+    XFree(data);
+  if (type)
+    return win;
+  inf = TryChildren(dpy, win, WM_STATE);
+  if (!inf)
+    inf = win;
+  return inf;
+}
+
+static Window
+TryChildren(Display *dpy, Window win, Atom WM_STATE)
+{
+  Window root, parent;
+  Window *children;
+  unsigned int nchildren;
+  unsigned int i;
+  Atom type = None;
+  int format;
+  unsigned long nitems, after;
+  unsigned char *data;
+  Window inf = 0;
+
+  if (!XQueryTree(dpy, win, &root, &parent, &children, &nchildren))
+    return 0;
+  for (i = 0; !inf && (i < nchildren); i++) {
+  data = NULL;
+  XGetWindowProperty(dpy, children[i], WM_STATE, 0, 0, False,
+         AnyPropertyType, &type, &format, &nitems,
+         &after, &data);
+  if (data)
+    XFree(data);
+  if (type)
+    inf = children[i];
+  }
+  for (i = 0; !inf && (i < nchildren); i++)
+  inf = TryChildren(dpy, children[i], WM_STATE);
+  if (children)
+    XFree(children);
+  return inf;
+}
+
 /**
  * @ingroup dnd
  */
@@ -364,7 +483,7 @@ DBM(cout << __FILE__ << ":" << __LINE__ << endl;)
       UpdateCursor();
     }
   } else {
-    Window w = XmuClientWindow(x11display, event.xmotion.subwindow);
+    Window w = toad::XmuClientWindow(x11display, event.xmotion.subwindow);
     if (x11_current_window != w) {
       if (inside_extern_window) {
         SendXdndLeave();
