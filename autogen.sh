cca4d7b3fc043d42752c0037b7f1d7afc2bee91f
Mark-Andre Hopf <mhopf@mark13.org>
2007-07-03 19:57:00 +0000

    added fallbacks for autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,10 @@
 #
 
 # add additional files for the LT_INIT macro
-libtoolize --force --copy   
-aclocal -I ./
+libtoolize --force --copy || 
+  cp autogen/config.guess \
+     autogen/config.sub \
+     autogen/ltmain.sh .
+aclocal -I ./ || cp autogen/aclocal.m4 .
 # create configure script
-autoconf
+autoconf || cp autogen/configure .
